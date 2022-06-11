34fdbd8476d60449cd00a62843ae37226f5ea0af
PrysmaticaX <62621539+Raghu-exe@users.noreply.github.com>
2022-06-11 09:03:17 -0400

    Add files via upload

---
+++ app.php
@@ -0,0 +1,43 @@
+
+
+
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Table Contents</title>
+</head>
+<body bgcolor="Azure">  
+    <div id="form" class=" col-lg-6 offset-lg-3 ">
+    
+        <form NAME ="database_search" ACTION="backend.php" METHOD="post">
+        <h3>  Please enter you query in the search field:  </h3>
+            <input name ="search" type="text" class="input" placeholder="Search...."/>
+
+            <input type="submit" name="search_button" Value="Submit Query"> </button>
+            <button id="clear" class="clear-results">Clear</button> 
+
+
+
+        <h4> please Select the type of Query </h4>
+
+
+        <input type="checkbox" name="checkbox" id="columns_gene" value="gene" checked="checked" />
+        <label for="column_gene">Gene</label>
+
+        <input type="checkbox" name="checkbox" id="column_disease" value="disease" />
+        <label for="column_disease">Disease</label>
+
+
+        </form>
+    </div>
+        
+    <?php 
+    require('./backend.php'); 
+
+    ?>
+
+
+</body>
+</html>
---
+++ backend.php
@@ -0,0 +1,89 @@
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Table Contents</title>
+</head>
+<body bgcolor="Azure">  
+<div>
+        <h2>Display Data using PHP/HTML & MySQL - Raghu Wable (test)</h2>
+
+
+        <style>
+        <?php include 'styling.css'; ?>
+        </style>
+
+
+
+    <style><?php include 'C:/Code/phpApp/styling.css'; ?></style>
+
+    </div>
+
+<?php 
+
+
+    $search = $_POST['search'];
+    
+
+
+    $host    = "localhost";
+    $user    = "root";
+    $pass    = "raghu";
+    $db_name = "icd";
+    
+    
+        //create connection
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $connection = mysqli_connect($host, $user, $pass, $db_name);
+
+
+    $sql = "SELECT * FROM icd_10";
+
+
+    $selection = $_POST['checkbox'];
+
+    if ( $selection == "gene" ) {
+        
+        $sql = "SELECT * FROM icd_10 WHERE gene LIKE '%$search%'";
+
+    } else if ( $selection == "disease") {
+
+        $sql = "SELECT * FROM icd_10 WHERE Disease LIKE '%$search%'";
+
+    }
+     
+    $result = mysqli_query($connection, $sql);
+
+
+    $all_property = array();  //declare an array for saving property
+
+
+
+    //showing property
+    echo '<table class="data-table">
+            <tr class="data-heading">';  //initialize table tag
+
+           
+    while ($property = mysqli_fetch_field($result)) {
+        echo '<td>' . $property->name . '</td>';  //get field name for header
+        $all_property[] = $property->name;  //save those to array
+    }
+    echo '</tr>'; //end tr tag
+
+    //showing all data
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        foreach ($all_property as $item) {
+            echo '<td>' . $row[$item] . '</td>'; //get items using property value
+        }
+        echo '</tr>';
+    }
+
+    echo "</table>";
+
+?>
+
+</body>
+</html>
+    
